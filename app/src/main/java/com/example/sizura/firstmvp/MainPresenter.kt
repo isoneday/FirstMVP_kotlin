e1c72920c8fc7178d93937edc5705ec060bab0c3
udakita <udakita11@gmail.com>
2018-10-31 22:08:18 +0700

    first commit 2

--- app/src/main/java/com/example/sizura/firstmvp/MainPresenter.kt
+++ app/src/main/java/com/example/sizura/firstmvp/MainPresenter.kt
@@ -3,7 +3,7 @@ package com.example.sizura.firstmvp
 class MainPresenter (var mainView: MainView){
 
     fun calculate (a: Int,b : Int){
-
+//hackto3
         val hasil = a + b
 //yuhu
         mainView.result(hasil)
